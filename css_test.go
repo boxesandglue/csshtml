9e4bb620ffa5bd72f1946a2c64ba50bffcffd96a
Patrick Gundlach <gundlach@speedata.de>
2024-07-04 10:59:03 +0200

    Remove CSS/HTML from boxes and glue
    The HTML stuff is now in a separate library

---
+++ css_test.go
@@ -0,0 +1,66 @@
+package csshtml
+
+import (
+	"testing"
+)
+
+func TestNestedAtrule(t *testing.T) {
+
+	str := `
+	@page {
+		size: a5;
+		@bottom-right-corner {
+			border: 4pt solid green;
+			border-bottom-color: rebeccapurple;
+		}
+
+		/* @top-left-corner {
+			border: 1pt solid green;
+			border-bottom-color: rebeccapurple;
+		} */
+
+	@top-right-corner {
+			border: 3pt solid green;
+			border-bottom-color: rebeccapurple;
+		}
+
+		@bottom-left-corner {
+			border: 2pt solid green;
+			border-bottom-color: rebeccapurple;
+		}
+
+	}`
+	toks := tokenizeCSSString(str)
+	bl := consumeBlock(toks, false)
+	if len(bl.ChildAtRules[0].ChildAtRules) != 3 {
+		t.Errorf("want 3 child @ rules, got %d", len(bl.ChildAtRules[0].ChildAtRules))
+	}
+}
+
+func TestFontFace(t *testing.T) {
+	str := `@font-face {
+		font-family: "Trickster";
+		src:
+		  local("Trickster"),
+		  url("trickster-COLRv1.otf") format("opentype") tech(color-COLRv1),
+		  url("trickster-outline.otf") format("opentype"),
+		  url("trickster-outline.woff") format("woff");
+	  }`
+	cp := NewCSSParser()
+	err := cp.AddCSSText(str)
+	if err != nil {
+		t.Error(err)
+	}
+	fontfaces := cp.FontFaces
+	if got, want := len(fontfaces), 1; got != want {
+		t.Errorf("len(c.FontFaces) = %d, want %d", got, want)
+	}
+	firstFontFace := fontfaces[0]
+	if want, got := 4, len(firstFontFace.Source); got != want {
+		t.Errorf("len(c.FontFaces[0].Source) = %d, want %d", got, want)
+	}
+	if want, got := "color-COLRv1", firstFontFace.Source[1].Tech; got != want {
+		t.Errorf(`firstFontFace.Source[1].Tech = %s, want %s`, got, want)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/speedata/csshtml
+
+go 1.18
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.1
+	github.com/andybalholm/cascadia v1.3.2
+	github.com/speedata/css/scanner v0.0.0-20201005071706-2ed32c3ce553
+	golang.org/x/net v0.22.0
+)
